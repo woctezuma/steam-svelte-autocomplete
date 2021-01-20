b690cac628ac3ffda91ca15c027dd8f95e7cbeae
Wok <woctezuma@users.noreply.github.com>
2021-01-20 13:36:21 +0100

    Data: a sample of 5 games

---
+++ src/game-list-sample.js
@@ -0,0 +1,22 @@
+export var gameList = [
+    {
+        id: 1,
+        name: "Counter-Strike"
+    },
+    {
+        id: 2,
+        name: "Team Fortress Classic"
+    },
+    {
+        id: 3,
+        name: "Day of Defeat"
+    },
+    {
+        id: 4,
+        name: "Deathmatch Classic"
+    },
+    {
+        id: 5,
+        name: "Half-Life: Opposing Force"
+    },
+];
